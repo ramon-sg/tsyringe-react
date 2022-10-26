75873ce5a3c32d185db7d392b80bfb30e5aee9d8
ramon <ramon@MacBook-Pro-m1-de-ramon.local>
2022-10-26 01:47:32 -0300

    Update example

--- example/src/components/CharacterCard.tsx
+++ example/src/components/CharacterCard.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import { FC } from 'react';
 import { Character } from '../entities/character';
 
@@ -8,7 +9,7 @@ type CharacterCardProps = {
 const CharacterCard: FC<CharacterCardProps> = ({ character }) => {
   return (
     <div className="bg-orange-500  p-2 relative flex justify-center">
-      <img src={character.imageUrl} />
+      <Image src={character.imageUrl} alt={`image ${character.name}`} />
       <div className="w-full h-10 absolute px-4 bottom-6 rotate-2">
         <div className="bg-white animate-bounce border-8 border-l-cyan-500 border-t-violet-500 border-r-teal-600 border-b-pink-500 px-2 py-1 text-sm">
           {character.name}
